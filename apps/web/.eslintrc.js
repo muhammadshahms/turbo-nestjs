73b33c4d34b0bc65f0a432bb5fc0f0db831e10ac
Muhammad Shah <muhammadshah4589@gmail.com>
2025-04-16 18:14:49 +0500

    changes

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-config/next.js'],
+  extends: [require.resolve('@repo/eslint-config/next.js')],
   parserOptions: {
-    project: true,
+    project: './tsconfig.json',
   },
 };
